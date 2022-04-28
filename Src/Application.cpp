db3c2c0e789b7f0cd33ffce9ca7f5d4e331d8e87
Emanuel Redondo <eredo001@ucr.edu>
2022-04-28 12:41:38 -0400

    First triangle

--- Src/Application.cpp
+++ Src/Application.cpp
@@ -3,7 +3,36 @@
 
 
 #include <iostream>
+#include <string>
 
+const char* Vshader =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "   void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "}\n";
+
+const char* Fshader =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);"
+    "}";
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void ProcessInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
 
 int main(void)
 {
@@ -18,7 +47,7 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(1920, 1080, "OpenGL Demos", NULL, NULL);
+    window = glfwCreateWindow(1200, 900, "OpenGL Demos", NULL, NULL);
     if (!window)
     {
         std::cout << "Failed to create window" << std::endl;
@@ -29,19 +58,96 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to intialize glad" << std::endl;
         return -1;
     }
 
+    glViewport(0, 0, 1200, 900);
+
+    unsigned int vshader, fshader, program;
+
+    vshader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vshader, 1, &Vshader, NULL);
+    glCompileShader(vshader);
+
+    char infolog[512];
+    int success;
+
+    glGetShaderiv(vshader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vshader, 512, NULL, infolog);
+        std::cout << "Failed to compile vertex shader. Error: " << infolog << std::endl;
+    }
     
+    fshader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fshader, 1, &Fshader, NULL);
+    glCompileShader(fshader);
+
+    glGetShaderiv(vshader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vshader, 512, NULL, infolog);
+        std::cout << "Failed to compile fragment shader. Error: " << infolog << std::endl;
+    }
+
+    program = glCreateProgram();
+    glAttachShader(program, vshader);
+    glAttachShader(program, fshader);
+    glLinkProgram(program);
+
+    glDeleteShader(vshader);
+    glDeleteShader(fshader);
+
+    glGetProgramiv(program, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(program, 512, NULL, infolog);
+        std::cout << "Failed to link program. Error: " << infolog << std::endl;
+    }
+
+    const float vertices[] =
+    {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f, 0.5f, 0.0f
+    };
+
+    unsigned int VAO;
+    unsigned int VBO;
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        /*check to see if escape key had been pressed*/
+        ProcessInput(window);
+
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.0f, 0.5f, 0.5f, 1.0f);
+
+        glBindVertexArray(VAO);
+        glUseProgram(program);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -49,6 +155,9 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+    
+    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &VAO);
 
     glfwTerminate();
     return 0;
